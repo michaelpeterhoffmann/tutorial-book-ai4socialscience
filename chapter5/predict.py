5c565aa7344fb6a65da83dcdf2cd8c031a37a528
Michael Hoffmann <54324880+michaelpeterhoffmann@users.noreply.github.com>
2023-11-14 11:39:58 +0100

    Update predict.py

--- chapter5/predict.py
+++ chapter5/predict.py
@@ -1,1 +1,18 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import pad_sequences
+import pickle
 
+VOCAB_SIZE=10000
+MAX_LEN = 250
+MODEL_PATH = 'sentiment_analysis_model.h5'
+
+#Load the tokenizer
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+def encode_texts(text_list):
+    encoded_texts = []
+    for text in text_list:
+        #do something
